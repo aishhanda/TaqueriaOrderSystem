2c6deade6168b7b7ed3779c33dd1a11c71055816
aishhanda <handaaishx@gmail.com>
2025-06-07 10:29:38 +0530

    Create taqueria.py

---
+++ taqueria.py
@@ -0,0 +1,30 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def main():
+
+    while True:
+        try:
+            items = input("Items: ").title()
+            result = bill(items)
+            
+        except EOFError:
+            print(result)
+            break
+
+
+def bill(items):
+    total = 0
+    if items in menu:
+        total += menu[items]
+        return f"Total: {total: .2f}"
+main()
